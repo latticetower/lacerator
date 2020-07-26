eb9c7f524e26df21c8c38587b3994176ae7be5b8
latticetower <merlettaia@gmail.com>
2020-07-26 20:33:47 +0300

    add template methods stubs

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from bottle import route, run, template
 import os
 from wfc import *
 
+@route('/pattern-generator/<h>/<w>')
+def generate_wfc(h, w):
+    return template("templates/pattern-generator.html",
+        svg=make_pattern_from_fragments(h, w))#'<b>Generator</b>'
+    #return template('<b>Hello {{name}}</b>!', name=name)
+
+
+@route('/simple-generator')
+def generate_wfc():
+    return template("templates/pattern-generator.html",
+        svg=make_pattern_measured(3))#'<b>Generator</b>'
+    #return template('<b>Hello {{name}}</b>!', name=name)
+
+
 @route('/')
 def index():
-    return template("templates/page.tpl", name="Tanya")#'<b>Generator</b>'
+    return template("templates/page.tpl", name=".")#'<b>Generator</b>'
     #return template('<b>Hello {{name}}</b>!', name=name)
 
 if os.environ.get('APP_LOCATION') == 'heroku':
