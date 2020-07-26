00a0eae8d4d7ffa3752789241ab7976debfecb28
latticetower <merlettaia@gmail.com>
2020-07-26 19:32:26 +0300

    set port as a param

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bottle import route, run, template
-
+import sys
 from wfc import *
 
 @route('/')
@@ -7,4 +7,5 @@ def index():
     return template("templates/page.tpl", name="Tanya")#'<b>Generator</b>'
     #return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='localhost', port=5000)
+port = 8080 if len(sys.argv)<2 else sys.argv[1]
+run(host='localhost', port=port)
