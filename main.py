1efc6217f769622d91df7f8a4192d7d0d3e7dc35
latticetower <merlettaia@gmail.com>
2020-07-26 14:56:02 +0300

    add wfc code - need fixing

---
+++ main.py
@@ -0,0 +1,10 @@
+from bottle import route, run, template
+
+from wfc import *
+
+@route('/')
+def index():
+    return template("templates/page.tpl", name="Tanya")#'<b>Generator</b>'
+    #return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=8080)
